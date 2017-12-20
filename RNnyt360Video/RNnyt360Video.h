1ec5aba1935c7bcbde28a049554fc95724228f28
adam-denny <adam@yourbrandlive.com>
2017-12-20 15:23:13 -0800

    Initial Commit

---
+++ RNnyt360Video/RNnyt360Video.h
@@ -0,0 +1,13 @@
+//
+//  RNnyt360Video.h
+//  RNnyt360Video
+//
+//  Created by Adam Denny on 12/20/17.
+//  Copyright © 2017 Brandlive, LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RNnyt360Video : NSObject
+
+@end
---
+++ RNnyt360Video/RNnyt360Video.m
@@ -0,0 +1,13 @@
+//
+//  RNnyt360Video.m
+//  RNnyt360Video
+//
+//  Created by Adam Denny on 12/20/17.
+//  Copyright © 2017 Brandlive, LLC. All rights reserved.
+//
+
+#import "RNnyt360Video.h"
+
+@implementation RNnyt360Video
+
+@end
